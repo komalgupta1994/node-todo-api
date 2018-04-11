ac34f7f185a1f6d8397883f528f76c4e707bd15f
Abhishek <abhishek@pc037.allcad.home>
2018-04-11 19:16:37 +0530

    change in server file

--- server/server.js
+++ server/server.js
@@ -3,24 +3,48 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
-    completedAt: {
-        type: Number
+// var Todo = mongoose.model('Todo', {
+//     text: {
+//         type: String,
+//         required: true,
+//         minlength: 1,
+//         trim: true
+//     },
+//     completed: {
+//         type: Boolean,
+//         default: false
+//     },
+//     completedAt: {
+//         type: Number,
+//         default: null
+//     }
+// });
+
+// var newTodo = new Todo({
+//     text: '   edit todo '
+// });
+
+// newTodo.save().then((doc) => {
+//     console.log('saved todo', doc)
+// }, (e) => {
+//     console.log('unable to save todo', e);
+// })
+
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     }
 });
 
-var newTodo = new Todo({
-    text: 'cook dinner'
+var user = new User({
+    email: 'komal@gmail.com'
 });
 
-newTodo.save().then((doc) => {
-    console.log('saved todo', doc)
+user.save().then((docs) => {
+    console.log('save', docs);
 }, (e) => {
-    console.log('unable to save todo', e);
+    console.log('error', e);
 })
